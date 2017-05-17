cd2d8d8fc67a8cf7065e8af9441fe681cb3082b2
Eric Sowell <eric.sowell@gmail.com>
2017-05-17 14:25:03 -0500

    I might have done routing okay but something may be wrong

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import RecipePuppySearch from './RecipePuppySearch.js';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SavedRecipes from './SavedRecipes.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <RecipePuppySearch />
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Search</Link>
+            <Link to="/saved">Saved Recipes</Link>
+          </nav>
+
+          <Route path="/" exact component={RecipePuppySearch} />
+          <Route path="/saved" component={SavedRecipes} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ react-ui/src/SavedRecipes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SavedRecipes extends React.Component {
+
+  render() {
+    return (
+      <div>
+        So saved recipes. Very food. Wow.
+      </div>
+    );
+  }
+
+}
+
+module.exports = SavedRecipes;
