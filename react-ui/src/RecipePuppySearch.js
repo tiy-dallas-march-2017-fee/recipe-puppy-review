94d13251c824afefb1ac646f81e3392adbdad19a
Eric Sowell <eric.sowell@gmail.com>
2017-05-17 10:10:55 -0500

    added query input

--- react-ui/src/RecipePuppySearch.js
+++ react-ui/src/RecipePuppySearch.js
@@ -3,6 +3,14 @@ import $ from 'jquery';
 
 class RecipePuppySearch extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      queryInputValue: '',
+      liveQueryValue: ''
+    }
+  }
+
   componentDidMount() {
     $.ajax({
       url: '/api/recipes'
@@ -12,10 +20,31 @@ class RecipePuppySearch extends React.Component {
     });
   }
 
+  handleChange(evt) {
+    this.setState({
+      queryInputValue: evt.target.value
+    });
+  }
+
+  handleKeyUp(evt) {
+    if (evt.keyCode === 13) {
+      this.setState({
+        liveQueryValue: evt.target.value,
+        queryInputValue: ''
+      });
+    }
+  }
 
   render() {
+    console.log('render state', this.state);
     return (
       <div>
+        <input
+          onChange={(evt) => this.handleChange(evt)}
+          onKeyUp={(evt) => this.handleKeyUp(evt)}
+          value={this.state.queryInputValue}
+          />
+
         RecipePuppySearch
       </div>
     );
