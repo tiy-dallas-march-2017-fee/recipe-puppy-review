8cb7ec210c2c9c25a214386ebdb0c208c5d68704
Eric Sowell <eric.sowell@gmail.com>
2017-05-17 09:37:23 -0500

    test change

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hey look, I changed this.
         </p>
       </div>
     );
